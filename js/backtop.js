53031d66b6bd84fd432ecf48f80b2ff325e12654
Kelvin_Yuu <kelvinu123456@gmail.com>
2023-01-11 10:58:16 +0800

    update

--- js/backtop.js
+++ js/backtop.js
@@ -14,11 +14,11 @@ $(document).ready(function() {
         });
 
         // scroll body to 0px on click
-        $('#back-top a').click(function() {
+        $('#back-top').click(function() {
             $('body,html').animate({
                 scrollTop: 0
             }, 800);
-            return false;
+            
         });
     });
 
